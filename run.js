c39bba8b3d7d2c521338bcd194543cd77a1d5e9a
Yuda Aditya <yudaaditya245@gmail.com>
2024-01-03 16:34:53 +0700

    add time for last scraps

--- run.js
+++ run.js
@@ -23,6 +23,25 @@ async function run() {
   
   console.log("\n=== FETCHING API ===\n");
   await fetchAPI();
+
+  logLastScrapTime()
 }
 
 run();
+
+function logLastScrapTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const ampm = hours >= 12 ? 'pm' : 'am';
+
+  // Convert hours to 12-hour format
+  const formattedHours = hours % 12 || 12;
+
+  // Add leading zero to minutes if needed
+  const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+
+  const lastScrapTime = `Last scrap: ${formattedHours}:${formattedMinutes} ${ampm}`;
+  
+  console.log('\n\n', lastScrapTime);
+}
